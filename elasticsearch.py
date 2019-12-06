b053748a7426288582bddb530a8585af1b4b32dd
Furong Guo <furong_g@163.com>
2019-12-06 16:54:31 +0100

    Update elasticsearch.py

--- elasticsearch.py
+++ elasticsearch.py
@@ -13,8 +13,14 @@ def search(domain, query):
         for hit in response.get('hits', {}).get('hits', []):
             freebase_label = hit.get('_source', {}).get('label')
             freebase_id = hit.get('_source', {}).get('resource')
-            id_labels.setdefault(freebase_id, set()).add( freebase_label )
+            freebase_score = hit.get('_score')
+            freebase_number_of_facts = 0
+            freebase_match = 0
+            freebase_similarity = 0
+            id_labels.setdefault(freebase_id, set()).add( freebase_label ).add(freebase_score)
+            .add(freebase_number_of_facts).add(freebase_match).add(freebase_similarity)
     return id_labels
+
 
 if __name__ == '__main__':
     import sys
