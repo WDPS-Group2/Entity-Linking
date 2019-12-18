f0e6b04d554833591f6e683927c118353060870c
Furong Guo <furong_g@163.com>
2019-12-18 12:28:14 +0100

    Update elasticsearch.py

--- elasticsearch.py
+++ elasticsearch.py
@@ -1,26 +1,38 @@
 import requests
-
+'''
+example
+"http://10.149.0.127:9200/freebase/label/_search?q=obama"
+{
+    "hits" : {
+    "total" : 172,
+    "max_score" : 6.014344,
+    "hits" : [ {
+      "_index" : "freebase",
+      "_type" : "label",
+      "_id" : "AVgvwvuvuV5CxvxAa2D1",
+      "_score" : 6.014344,
+      "_source" : {
+        "resource" : "fbase:m.02mjmr",
+        "label" : "Barack Obama"
+      }
+    }
+'''
 
 def search(domain, query):
     url = 'http://%s/freebase/label/_search' % domain
     try:
-        response = requests.get(url, params={'q': query, 'size':20})
+        response = requests.get(url, params={'q': query, 'size':10})
     except:
         return {}
-    id_labels = {}
+    id_score_labels = {}
     if response:
         response = response.json()
         for hit in response.get('hits', {}).get('hits', []):
             freebase_label = hit.get('_source', {}).get('label')
             freebase_id = hit.get('_source', {}).get('resource')
             freebase_score = hit.get('_score')
-            freebase_number_of_facts = 0
-            freebase_match = 0
-            freebase_similarity = 0
-            id_labels.setdefault(freebase_id, set()).add( freebase_label ).add(freebase_score)
-            .add(freebase_number_of_facts).add(freebase_match).add(freebase_similarity)
-    return id_labels
-
+            id_score_labels.setdefault(freebase_id, set()).add(freebase_score).add( freebase_label )
+    return id_score_labels
 
 if __name__ == '__main__':
     import sys
